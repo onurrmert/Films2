f00a81f08caa32881e75d8da3c0bec9472be2041
onur mert boybek <onurboybek@gmail.com>
2023-01-31 18:00:56 +0300

    First commit-1

--- app/src/main/java/com/onurmert/filmsjava/MainActivity.java
+++ app/src/main/java/com/onurmert/filmsjava/MainActivity.java
@@ -6,12 +6,10 @@ import com.onurmert.filmsjava.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
     }
 }
